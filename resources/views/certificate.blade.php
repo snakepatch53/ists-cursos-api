3324f934c21a5bee07a7beed67650af135698df0
snakepatch53 <snakepatch53@gmail.com>
2024-01-17 18:31:43 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   resources/views/certificate.blade.php

--- resources/views/certificate.blade.php
+++ resources/views/certificate.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>{{ $course->name }}</title>
+    <title>{{ $inscription->$course->name }}</title>
 </head>
 
 <body class="antialiased">
