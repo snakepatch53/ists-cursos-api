98ed2063ca8c42df9d9a8c59b7c7fcbc3cb51034
snakepatch53 <snakepatch53@gmail.com>
2024-01-17 21:01:43 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   database/seeders/TemplateSeeder.php

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Template;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TemplateSeeder extends Seeder
 {
@@ -15,6 +16,18 @@ class TemplateSeeder extends Seeder
      */
     public function run()
     {
-        Template::factory()->count(5)->create();
+        // Template::factory()->count(5)->create();
+        DB::table('templates')->insert([
+            'name' => 'Template 1',
+            'code' => '
+                <img src="{{fondo_certificado1}}"   class="fixed inset-0 -top-3"  style="transform:scale(1.08)"/>
+                <div class="relative">
+                    <div class="mt-80" /><div class="mt-7" />
+                    <h3 class="font-arial font-bold text-center text-4xl uppercase opacity-90">{{student.name}} {{student.lastname}}</h3>
+                    <div class="mt-4" />
+                    <p class="font-arial text-justify px-12 leading-6 opacity-85" style="font-size:1.45rem"> Por haber aprobado el curso de: <b>{{course.name}}</b>, dictado por el {{teacher.name}} {{teacher.lastname}}, en la ciudad de Sucúa, desde el 12 de junio al 23 de julio del 2023, con una duración de <b>{{course.duration}} horas académicas</b>. </p>
+                </div>
+            ',
+        ]);
     }
 }
