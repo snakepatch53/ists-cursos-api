e778ce5c4b616ee0e944229d29ec246ca1513a43
snakepatch53 <snakepatch53@gmail.com>
2024-01-19 15:31:48 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/certificate/{id}', InscriptionController::class . '@certificate')->name('certificate');
 Route::get('/get-insciptions-cetec-register-excel/{course_id}', ComboController::class . '@getInsciptionsCetecRegisterExcel')->name('getInsciptionsCetecRegisterExcel');
-Route::get('/get-insciptions-cetec-approveds-excel/{course_id}', ComboController::class . '@getInscriptionCetecApprovedsExport')->name('getInsciptionsCetecRegisterExcel');
+Route::get('/get-insciptions-cetec-approveds-excel/{course_id}', ComboController::class . '@getInscriptionCetecApprovedsExport')->name('getInscriptionCetecApprovedsExport');
 Route::get('/get-inscription-moodle-csv-export/{course_id}', ComboController::class . '@getInscriptionMoodleCsvExport')->name('getInscriptionMoodleCsvExport');
